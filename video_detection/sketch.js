9a3e384ba9adf696d2a32c81f762b85e6c32b416
adonai <adonai@in8.com.br>
2023-01-12 12:12:32 -0300

    video detection

--- video_detection/sketch.js
+++ video_detection/sketch.js
@@ -1,28 +1,20 @@
 let video;
 let detector;
+let objectDetected = [];
 
 function preload() {
 	detector = ml5.objectDetector('cocossd', {}, modelLoaded);
 }
 
 function gotDetection() {
-	detector.detect(video, (err, results) => {
-		for (let i = 0; i < results.length; i++) {
-			let object = results[i];
-			stroke(0, 255, 0);
-			strokeWeight(4);
-			noFill();
-			rect(object.x, object.y, object.width, object.height)
-			noStroke();
-			fill(0, 255, 0);
-			textSize(24);
-			text(object.label, object.x + 10, object.y + 24);
-		}
-	});
 
+	detector.detect(video, (err, results) => {
+		objectDetected = results
+	});
+	
 	setTimeout(() => {
 		return gotDetection()
-	}, 1);
+	}, 200);
 }
 
 function modelLoaded() {
@@ -40,4 +32,17 @@ function setup() {
 
 function draw() {
 	image(video, 0, 0)
+	if (objectDetected?.length > 0) {
+		for (let i = 0; i < objectDetected.length; i++) {
+			let object = objectDetected[i];
+			stroke(0, 255, 0);
+			strokeWeight(4);
+			noFill();
+			rect(object.x, object.y, object.width, object.height)
+			noStroke();
+			fill(0, 255, 0);
+			textSize(24);
+			text(object.label, object.x + 10, object.y + 24);
+		}
+	}
 }
